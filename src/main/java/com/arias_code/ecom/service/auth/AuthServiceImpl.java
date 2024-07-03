80691153af63fd1d06e1b81f8041ee7dc1c77e22
alexArias96 <alexherdez3092@gmail.com>
2024-07-03 16:21:12 -0600

    BACK-ECOM-ARIAS-04: Adding logic to the shopping cart part with the entities: order() and cartItems()

--- src/main/java/com/arias_code/ecom/service/auth/AuthServiceImpl.java
+++ src/main/java/com/arias_code/ecom/service/auth/AuthServiceImpl.java
@@ -2,8 +2,11 @@ package com.arias_code.ecom.service.auth;
 
 import com.arias_code.ecom.dto.SignupDTO;
 import com.arias_code.ecom.dto.UserDto;
+import com.arias_code.ecom.entity.Order;
 import com.arias_code.ecom.entity.User;
+import com.arias_code.ecom.enums.OrderStatus;
 import com.arias_code.ecom.enums.UserRole;
+import com.arias_code.ecom.repository.OrderRepository;
 import com.arias_code.ecom.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +20,28 @@ public class AuthServiceImpl implements AuthService{
     private UserRepository userRepository;
     private BCryptPasswordEncoder passwordEncoder;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     public UserDto createUser(SignupDTO signupDTO){
         User user = new User();
         user.setName(signupDTO.getName());
         user.setEmail(signupDTO.getEmail());
         user.setPassword(new BCryptPasswordEncoder().encode(signupDTO.getPassword()));
         user.setRole(UserRole.CUSTOMER);
-
         User createdUser = userRepository.save(user);
+
+        /*
+         * when the user is created the initial order is also created
+         */
+        Order order = new Order();
+        order.setAmount(0L);
+        order.setTotalAmount(0L);
+        order.setDiscount(0L);
+        order.setUser(createdUser);
+        order.setOrderStatus(OrderStatus.PENDING);
+        orderRepository.save(order);
+
         UserDto userDto = new UserDto();
         userDto.setId(createdUser.getId());
 
